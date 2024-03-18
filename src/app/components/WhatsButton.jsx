d5965330a458628bfac85df39ef42e70323d0fcd
Caique <asapcaique7@gmail.com>
2024-03-18 14:31:12 -0300

    configurada api pra deploy

--- src/app/components/WhatsButton.jsx
+++ src/app/components/WhatsButton.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function WhatsButton() {
     return(
         <button className="fixed bottom-4 right-4 rounded-full p-2 outline-none z-1000">
-                <Link href="https://wa.me/1299185-5821" className="text-green-400"><PiWhatsappLogoDuotone size={70}/></Link>
+                <Link href="https://wa.me/1299185-5821" target="_blank" className="text-green-400"><PiWhatsappLogoDuotone size={70}/></Link>
         </button>
     )
 }
